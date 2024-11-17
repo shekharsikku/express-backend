348ade4d68d988ae8dbc64e6313198776592cd78
Shekhar Sharma <shekharsharma.ss9151@gmail.com>
2024-11-17 09:10:21 +0530

    User model all fields verified

--- src/model/user.ts
+++ src/model/user.ts
@@ -1,50 +1,74 @@
-import { Document, Schema, Types, model } from "mongoose";
+import { Types, Schema, model } from "mongoose";
+import { UserInterface } from "../interface";
 
-interface UserInterface extends Document {
-  _id: Types.ObjectId;
-  fullname: string;
-  username: string;
-  email: string;
-  password: string;
-  refreshtoken?: string;
-}
-
-/**
- * UserSchema for define UserModel.
- *
- * @types {UserInterface}
- *
- * @property {string} fullname - The user's fullname.
- * @property {string} username - The user's username.
- * @property {string} email - The user's email.
- * @property {string} password - The user's password.
- * @property {string} refreshtoken - The user's refreshtoken.
- */
 const UserSchema = new Schema<UserInterface>(
   {
-    fullname: {
+    name: {
       type: String,
-      required: true,
-    },
-    username: {
-      type: String,
-      required: true,
-      lowercase: true,
-      unique: true,
+      default: null,
     },
     email: {
       type: String,
       required: true,
       unique: true,
     },
+    username: {
+      type: String,
+      unique: true,
+      lowercase: true,
+      sparse: true,
+      default: null,
+    },
     password: {
       type: String,
       required: true,
       select: false,
     },
-    refreshtoken: {
+    gender: {
       type: String,
+      enum: ["Male", "Female", "Other"],
+      default: null,
+    },
+    image: {
+      type: String,
+      default: null,
+    },
+    bio: {
+      type: String,
+      default: null,
+    },
+    setup: {
+      type: Boolean,
+      default: false,
+    },
+    authentication: {
+      type: [
+        {
+          token: String,
+          expiry: Date,
+          device: {
+            type: String,
+            default: null,
+          },
+        },
+      ],
       select: false,
+    },
+    verified: {
+      type: Boolean,
+      default: false,
+    },
+    active: {
+      type: Date,
+      default: Date.now,
+    },
+    verification: {
+      code: { type: String },
+      expiry: { type: Date },
+    },
+    reset: {
+      code: { type: String },
+      expiry: { type: Date },
     },
   },
   {
@@ -52,7 +76,12 @@ const UserSchema = new Schema<UserInterface>(
   }
 );
 
-export { UserInterface, UserSchema };
+UserSchema.pre("save", function (next) {
+  if (!this.username || this.username.trim() === "") {
+    this.username = new Types.ObjectId().toString();
+  }
+  next();
+});
 
 const User = model<UserInterface>("User", UserSchema);
 
