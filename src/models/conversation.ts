7ffc9bed947ce1fb37cf12cd47b5bbe6041263b0
Shekhar Sharma <shekharsharma.ss9151@gmail.com>
2025-02-11 11:12:00 +0530

    Model of conversation/messages completed

---
+++ src/models/conversation.ts
@@ -0,0 +1,32 @@
+import { ConversationInterface } from "../interface";
+import { Schema, model } from "mongoose";
+
+const ConversationSchema = new Schema<ConversationInterface>(
+  {
+    participants: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    messages: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Message",
+        default: [],
+      },
+    ],
+    interaction: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { timestamps: true }
+);
+
+const Conversation = model<ConversationInterface>(
+  "Conversation",
+  ConversationSchema
+);
+
+export default Conversation;
